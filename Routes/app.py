40d1d3e9093d6eeb9bb05f10eff64d2f09a15f92
OrlandLleshi <orlandlleshi@hotmail.co.uk>
2021-06-21 08:58:22 +0000

    updated tutorials

--- Routes/app.py
+++ Routes/app.py
@@ -11,5 +11,12 @@ def home():
 def about():
     return 'This is the about page'
 
+
+@app.route('/squared/<number>')
+def squared(number):
+    number = int(number)
+    result = number * number
+    return f'{str(number)} squared is equal to {str(result)}'
+
 if __name__ == "__main__":
     app.run(debug=True)
